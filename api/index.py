2c9c401a1b54b1ac22c899e2c098fde3d2afafb4
Nacode2014 <Private.nafi2016@gmail.com>
2022-09-05 19:43:44 +0700

    first adding

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from w3lib.url import url_query_cleaner
 
 app = Flask(__name__)
 
@@ -6,6 +7,21 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, world'
+
+@app.route("/clean", methods=["GET"])
+def clean():
+    if not request.args.get("url"):
+        return
+    query = request.args.get("url")
+    try:
+        if "music.amazon" in query:
+            if request.args.get("track", None) in query:
+                clean = url_query_cleaner(query, ['marketplaceId', "ref", "musicTerritory"], remove=True)
+                return {"Success": True, "data": clean}
+            else:
+                return "Belum di implementasi"
+    except Exception:
+        return "Beberapa fitur belum di masukan "
 
 
 @app.route('/test')
